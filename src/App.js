53045229d9999c4230c72c9e5a7f860587e2f559
Fernando Garrote De La Macorra <42215154+FernandoGarrote@users.noreply.github.com>
2021-09-07 21:16:11 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 //librerias ó components
 import "./App.css";
 import "./index.css";
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   
-  const [users, setUsers] = useState([{ id: 0, name: "Ruben" }]);
+  const [users, setUsers] = useState([{ id: 0, name: "Ansu Fati" }]);
 
-  const onClick = () => {};  
+  const onClick = () => {
+    setUsers([...users, {id: users.length, name:"Pedri Gonzalez"}])
+  };
+
+  var dataComponent = {
+    type:"button",
+    value:"Update"
+  };
+
+  const [seconds, setSeconds] = useState(0)
+  const [date, setDate] = useState(new Date());
+
+
+  useEffect (() => {
+    var timer = setInterval(() => tick(), 1000);
+
+    return function cleanup(params) {
+      clearInterval(timer)
+    }
+  });
+
+  useEffect (() => {
+    let interval = null;
+    interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);}, 1000);
+
+      return function cleanup(params) {
+        clearInterval(seconds)
+      }
+  })
+
+  function tick() {
+    setDate(new Date())
+  }
 
   return (
     <div>
-      <input type="button" onClick={onClick} value="Update" />
+      {}
+      <h1>{seconds}</h1>
+      <h1>{date.toLocaleTimeString()}</h1>
+      {}
+      <input onClick={onClick} {...dataComponent} />
       <div>
         {users.map((e) => (
           <div>
